af62a4de25644120cffd2c22585d43b8565894ec
play.js <git@gituser.com>
2020-12-30 07:33:59 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs=require("fs")
+
+const apfilename="./MyAirports.json"
+const jsonString = fs.readFileSync(apfilename)
+var myAirports = JSON.parse(jsonString) 
+
+function replacer(name,value)
+{
+  if (name=="latitude" || name=="longitude")
+  {
+    value=Number(value).toFixed(6);
+    return value;
+  }
+  return value;
+}
+
+var jsonAirports = JSON.stringify(myAirports,replacer)
+  fs.writeFileSync("./MyAirports2.json", jsonAirports)
+   
